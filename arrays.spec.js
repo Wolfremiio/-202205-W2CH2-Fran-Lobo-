66778a6af908b10deb73161093e06943cc38b73d
FranLobo <floboflores@gmail.com>
2022-05-18 19:47:40 +0200

    Add more functions

--- arrays.spec.js
+++ arrays.spec.js
@@ -1,4 +1,13 @@
-import { length, pop, push, shift } from './arrays.js';
+import {
+    every,
+    length,
+    pop,
+    push,
+    shift,
+    some,
+    unshift,
+    find,
+} from './arrays.js';
 
 const noIsArray = 'string';
 
@@ -33,7 +42,7 @@ describe('Given length function', () => {
 describe('Given push function', () => {
     describe('When array have 2 elements', () => {
         test('it should show 3 elements', () => {
-             // ARRANGE
+            // ARRANGE
 
             const array = ['pepe', 2];
             const element = 'pepa';
@@ -57,11 +66,10 @@ describe('Given push function', () => {
         });
     });
 });
-
 describe('Given pop function', () => {
     describe('When array have 3 elements', () => {
         test('it should show the dropped element', () => {
-             // ARRANGE
+            // ARRANGE
 
             const array = ['pepe', 2, 12];
             const expectedResult = 12;
@@ -76,7 +84,7 @@ describe('Given pop function', () => {
 
     describe('When array have 3 elements', () => {
         test("should show array's length -> 2 ", () => {
-             // ARRANGE
+            // ARRANGE
 
             let array = ['pepe', 2, 12];
             const expectedResult = 2;
@@ -104,7 +112,7 @@ describe('Given pop function', () => {
 describe('Given shift function', () => {
     describe('When array have 3 elements', () => {
         test("should show array's length -> 4", () => {
-             // ARRANGE
+            // ARRANGE
 
             let array = ['pepe', 2, 12];
             const expectedResult = 4;
@@ -125,6 +133,81 @@ describe('Given shift function', () => {
 
             // ASSERT
             expect(() => push(noIsArray)).toThrow(Error);
+        });
+    });
+});
+
+describe('Given unshift function', () => {
+    describe('When array have 3 elements', () => {
+        test('it should show the new array length', () => {
+            // ARRANGE
+
+            const array = ['pepe', 2, 12];
+            const expectedResult = 2;
+            // ACT
+            const result = unshift(array);
+
+            // ASSERT
+
+            expect(result).toBe(expectedResult);
+        });
+    });
+    describe('When no array is given', () => {
+        test('it should return error ', () => {
+            // ARRANGE
+
+            // ACT
+
+            // ASSERT
+            expect(() => push(noIsArray)).toThrow(Error);
+        });
+    });
+    describe('When function is given', () => {
+        test('it should return true ', () => {
+            // ARRANGE
+            const array = [3, 2, 3, 4];
+            function mayor(elem) {
+                return elem > 10;
+            }
+
+            const result = some(array, mayor);
+            // ACT
+
+            // ASSERT
+            expect(result).toBe(false);
+        });
+    });
+
+    describe('When function is given', () => {
+        test('it should return error ', () => {
+            // ARRANGE
+            const array = [12, 22, 33, 44];
+            function mayor(elem) {
+                return elem > 10;
+            }
+
+            const result = every(array, mayor);
+            // ACT
+
+            // ASSERT
+            expect(result).toBe(true);
+        });
+    });
+
+    describe('When function find element', () => {
+        test('it should return true ', () => {
+            // ARRANGE
+            const array = [12, 22, 33, 44];
+            function mayor(elem) {
+                return elem > 20;
+            }
+
+            const result = find(array, mayor);
+
+            // ACT
+
+            // ASSERT
+            expect(result).toBe(22);
         });
     });
 });
